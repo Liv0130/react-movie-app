64611139a69b6be3c5b85ca2effc3f80ab27598d
Liv0130 <yjjhsh27@gmail.com>
2022-05-07 22:27:06 +0900

    Sorted data by rating

--- src/Movie.js
+++ src/Movie.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Movie() {
-  return <h1>Helllo</h1>
+function Movie({id, title, year, summary, poster}) {
+  return <h4>{title}</h4>
 }
 
-Movie.propTypes = {};
+Movie.propTypes = { 
+  id: PropTypes.number.isRequired, 
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired
+};
 
 export default Movie;
